5b1d5cfa93de6ef4ac5e971f7cf906b459caf57a
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-05-02 14:15:06 -0400

    Added in a code (and tested it briefly) to generate a PDF to be used for undersampling in the simplest case. This undersampling pattern does not have any specification for directional data

--- recon_CS.py
+++ recon_CS.py
@@ -43,3 +43,112 @@ def zpad(res_sz,orig_data):
     padval = np.ceil((res_sz-orig_sz)/2)
     res = np.pad(orig_data,([padval[0],padval[0]],[padval[1],padval[1]]),mode='constant')
     return res
+    
+def genPDF(img_sz,
+        p,
+        pctg,
+        l_norm = 2,
+        radius = 0,
+        cyl = [0],
+        disp = 0):
+    """
+    Generates a Probability Density Function for Pseudo-undersampling (and potentially for use with the scanner after the fact. 
+    
+    This uses a variable density undersampling, allowing for more (or less) data in the centre
+    
+    Input:
+        [int list] img_sz - The size of the input dataset
+        [int]         p - polynomial power (1/r^p)
+        [float]    pctg - Sampling factor -- how much data we want to collect
+        [int]    l_norm - L1 or L2 distance measure
+        [float]  radius - fully sampled centre radius
+        [int list]  cyl - Is it cylindrical data acquisition? 
+        [bool]     disp - Do you want it displayed or not?
+    
+    Output:
+        2D[float] pdf - The probability density function
+        
+    Based on Lustig's work from 2007
+    """
+    
+    minval = 0.0
+    maxval = 1.0
+    val = 0.5
+    
+    # Check if we're doing cylindrical data, if so, change img_sz and note that we need to zero pad
+    if cyl[0] == 1:
+        img_sz_hold = [cyl[1], cyl[2]]
+        cir = True;
+        
+        if img_sz_hold != img_sz:
+            zpad_mat = True;
+        else:
+            zpad_mat = False;
+            
+    else:
+        img_sz_hold = img_sz
+        zpad_mat = False;
+    
+    # If the size vector is only one value, add on another value
+    if len(img_sz_hold) == 1:
+        img_sz_hold = [img_sz_hold, 1]
+    
+    
+    # How many of the datapoints do we have to look at?
+    sx = img_sz_hold[0]
+    sy = img_sz_hold[1]
+    PCTG = int(np.floor(pctg*sx*sy))
+    
+    if sum(img_sz_hold == 1) == 0: #2D case
+        [x,y] = np.meshgrid(np.linspace(-1,1,sy),np.linspace(-1,1,sx))
+        if l_norm == 1:
+            r = abs(np.array([x,y])).max(0)
+        elif l_norm == 2:
+            r = np.sqrt(x**2 + y**2)
+            # Check if the data is cyl acquisition -- if so, make sure outside datapoints don't get chosen by setting r = 1
+            if cyl[0]:
+                r[np.where(r > 1)] = 1
+            else:
+                r = r/r.max()
+    else: #1D
+        r = abs(np.linspace(-1,1,max([sx,sy])))
+        
+    idx = np.where(r < radius)
+    pdf = pdf = (1-r)**p
+    pdf[idx] = 1
+    
+    if np.floor(np.sum(pdf))>PCTG:
+        raise NameError('Polynomial too low. Would need to undersample DC. Increase P')
+    
+    # Bisect the data to get the proper PDF values to allow for the optimal sampling pattern generation
+    while(1):
+        val = minval/2 + maxval/2;
+        pdf = (1-r)**p + val
+        pdf[np.where(pdf > 1)] = 1
+        pdf[idx] = 1
+        N = np.floor(np.sum(pdf));
+        if N > PCTG:
+            maxval = val
+        elif N < PCTG:
+            minval = val;
+        else:
+            break;
+    
+    if zpad_mat:
+        pdf = zpad(img_sz,pdf)
+    
+    if disp:
+        plt.figure
+        plt.subplot(211) 
+        plt.imshow(pdf)
+        plt.subplot(212)
+        if np.sum(img_sz_hold == 1) == 0:
+            plt.plot(pdf[len(pdf)/2:,])
+        else:
+            plt.subplot(212)
+            plt.plot(pdf)
+        
+        plt.show()
+        
+    return pdf
+    
