fbe4c8a42523c8b4f38eca16a3fba3db48c7f033
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-05-03 15:30:50 -0400

    Created an easy way for transforms to be done quickly. Need to add in TV

---
+++ transforms.py
@@ -0,0 +1,28 @@
+#   transforms.py
+#
+#
+#
+#   Code to be a wrapper for all of the transforms that are done in order to clean up the central codes
+
+import numpy as np
+import numpy.fft as fft
+from rwt import dwt, idwt
+from rwt.wavelets import daubcqf
+
+def fft2c(data_to_fft):
+    FFTdata = 1/np.sqrt(data_to_fft.size)*fft.fftshift(fft.fft2(data_to_fft))
+    return FFTdata
+
+def ifft2c(data_to_ifft):
+    IFFTdata = np.sqrt(data_to_ifft.size)*fft.ifftshift(fft.ifft2(data_to_ifft))
+    return IFFTdata
+
+def xfm(data_to_xfm,scaling_factor = 4,L = 2):
+    h = daubcqf(scaling_factor)[0]
+    XFMdata = dwt(data_to_xfm,h,L)
+    return XFMdata
+
+def ixfm(data_to_ixfm,scaling_factor = 4,L = 2):
+    h = daubcqf(scaling_factor)[0]
+    IXFMdata = idwt(data_to_ixfm,h,L)
+    return IXFMdata
