6d6904c9eb9c46fa7cb39ad25c71e5004bf571ae
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-05-16 16:05:32 -0400

    Finished getting the directional term up and running. Need to now make it functional in the transform code. Currently, the output that the directional function gives is ndirs x imX x imY. Only works for 2D slices, but can be modified in the future for 3D pretty easily.

--- direction.py
+++ direction.py
@@ -9,6 +9,9 @@ from __future__ import division
 import numpy as np
 import numpy.fft as fft
 import scipy.optimize as sciopt
+from numpy.linalg import inv
+
+
 
 def dot_product_threshold_with_weights(filename,
                           threshold = 0.1,
@@ -32,7 +35,7 @@ def dot_product_threshold_with_weights(filename,
     return locs, vals
 
 def dot_product_with_mins(filename,
-                          nmins = 4)
+                          nmins = 4):
     '''
     This code exists to quickly calculate the closest directions in order to quickly get the values we need to calculate the mid matrix for the least squares fitting
     '''
@@ -63,40 +66,54 @@ def calc_Mid_Matrix(filename,nmins):
     Where A is from (I_{ijkr} - I_{ijkq}) = A_rq * B_{ijkq}
     Note that there is a different M for each direction that we have to parse through
     
-    The return is an m x 3 x 3 matrix where m is the number of directions that we have in the dataset.
+    The return is an nDirs x nMins x nMins matrix where m is the number of directions that we have in the dataset.
     '''
     inds,dirs = dot_product_with_mins(filename,nmins)
     #dirs = np.loadtxt(filename)
     
-    for i in xrange(dirs.shape[0]):
-        
+    M = np.zeros([dirs.shape[0],nmins,nmins])
     
+    for qDir in xrange(dirs.shape[0]):
+        A = np.array([])
+        A.shape = (nmins,0)
+        for dirComp in xrange(dirs.shape[1]):
+            datHold = np.reshape(dirs[inds[qDir,:],dirComp]-dirs[qDir,dirComp],(nmins,1))
+            datHold = datHold/np.linalg.norm(datHold) # Should I do this? Normalizes the vectors
+            A = np.hstack([A, datHold])
+        # I apologize for how messy this is
+        Ahat = np.dot(inv(np.dot(A.T,A)),A.T)
+        M[qDir,:,:] = np.dot(Ahat.T,Ahat)
+    
+    return M
+        
     
 #def residuals(a,b):
 #    return 
     
-def least_Squares_Fitting(Ir,Iq,r,q,filename):
+def least_Squares_Fitting(data,strtag,dirs,inds,M):
     
-    dirs = np.loadtxt(filename)
-    r = np.hstack([dirs[r,:],np.ones([len(r),1])])
-    q = np.hstack([dirs[q,:],1])
+    nmins = inds.shape[1]
+    dirloc = strtag.index("diff")
+    data = np.rollaxis(data,dirloc)
     
-    nrow, ncol = Iq.shape
+    for q in xrange(dirs.shape[0]):
+        r = inds[q,:]
     
-    A = np.zeros(np.hstack([r.shape,3]))
-    Irq = np.zeros(Ir.shape)
+        # Assume the data is coming in as image space data and pull out what we require
+        Iq = data[q,:,:]
+        Ir = data[r,:,:]
+        nrow, ncol = Iq.shape
     
-    for i in xrange(r.shape[0]):
-        Irq[:,:,i] = Ir[:,:,i] - Iq
-        A[i,:] = r[i,:] - q
+        #A = np.zeros(np.hstack([r.shape,3]))
+        Irq = Ir - Iq.reshape(1,nrow,ncol).repeat(nmins,0)
     
-    A = np.matrix(A);
-    Aleft = np.linalg.solve((A.T*A),A.T)
-    beta = np.zeros(np.hstack([Iq.shape,3]))
+        #Aleft = np.linalg.solve((A.T*A),A.T)
+        #beta = np.zeros(np.hstack([Iq.shape,3]))
+        
+        Gdiffsq = np.zeros(np.hstack([dirs.shape[0], nrow, ncol]))
+        
+        for i in xrange(nrow):
+            for j in xrange(ncol):
+                Gdiffsq[q,i,j] = np.dot(np.dot(Irq[:,i,j].reshape(1,nmins),M[q,:,:].reshape(nmins,nmins)),Irq[:,i,j].reshape(nmins,1))
     
-    for i in xrange(nrow):
-        for j in xrange(ncol):
-            beta[i,j,:] = Aleft*np.matrix(Irq[i,j,:].flat)
-            Gdiffsq[i,j] = 
-    
-    return beta, Gdiffsq
+    return Gdiffsq
