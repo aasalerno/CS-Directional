f2904c26b959f0b7a21c17914d54fcbdffc591e5
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-05-16 17:01:35 -0400

    Wrote in some code that should make us have all of the things that we need to make the other codes work

--- recon_CS.py
+++ recon_CS.py
@@ -19,6 +19,7 @@ import transforms as tf
 import scipy.ndimage.filters
 import grads
 import sampling as samp
+import direction as d
 
 EPS = np.finfo(float).eps
 
@@ -36,6 +37,7 @@ def phase_Calculation(data,is_kspace = 0,is_fftshifted = 0):
     return filtdata.conj()/(abs(filtdata)+EPS)
 
 def recon_CS(filename = '/home/asalerno/Documents/pyDirectionCompSense/data/SheppLogan256.npy',
+             strtag = ['diff','spatial','spatial'],
              TVWeight = 0.01,
              XFMWeight = 0.01,
              TVPixWeight = 1,
@@ -44,10 +46,11 @@ def recon_CS(filename = '/home/asalerno/Documents/pyDirectionCompSense/data/Shep
              ItnLim = 150,
              epsilon = 0.02,
              l1smooth = 1e-15,
-             xfmNorm = 1):
+             xfmNorm = 1,
+             dirFile = '/home/asalerno/Documents/pyDirectionCompSense/data/GradientVectorMag.txt',
+             nmins = 4):
     
     np.random.seed(2000)
-    
     im = np.load(filename)
     im = im + 0.1*(np.random.normal(size=[256,256]) + 1j*np.random.normal(size = [256,256])) # For the simplest case right now
 
@@ -59,9 +62,13 @@ def recon_CS(filename = '/home/asalerno/Documents/pyDirectionCompSense/data/Shep
     xfmWeight = 0.01 # Weight for Transform L1 penalty
     Itnlim = 8 # Number of iterations
     
-    
     pdf = samp.genPDF(N,P,pctg,radius = 0.1,cyl=[0])
     k = samp.genSampling(pdf,10,60)[0]
+    
+    # Diffusion information that we need
+    if 'dirFile' in locals():
+        dirs = np.loadtxt(dirFile)
+        M = d.calc_Mid_Matrix(dirs,nmins=4)
     
     data = np.fft.ifftshift(k)*tf.fft2c(im)
     #ph = phase_Calculation(im,is_kspace = False)
@@ -70,12 +77,14 @@ def recon_CS(filename = '/home/asalerno/Documents/pyDirectionCompSense/data/Shep
     # "Data from the scanner"
     im = tf.ifft2c(data)
     
-    # Primary first guess. What we're using for now.
+    # Primary first guess. What we're using for now. Density corrected
     im_dc = tf.ifft2c(data/np.fft.ifftshift(pdf)) 
+    
+    
     
     # Grads
     gObj = grads.gObj(im,data,k)
-    #gTV = grads.gTV(im)
+    gTV = grads.gTV(im,strtag,dirs,M)
     gXFM = grads.gXFM(im)
     
     
