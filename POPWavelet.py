d867e0e1a07b6e810cffdb7d3c3e5b2f9ab4cbc5
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-06-21 11:33:08 -0400

    First commit for this code, we're going to be using this to check how the code is calculating the wavelets

---
+++ POPWavelet.py
@@ -0,0 +1,9 @@
+'''
+Wavelet checking code!
+
+Let's use this code to check how the wavelets are calculated and things like that.
+
+We will be using my phantom -- with Daubeauchies and Haar wavelets.
+
+'''
+
