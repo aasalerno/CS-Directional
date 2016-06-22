238122ee7c8a292d2fa55283f14095057e0c28e6
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-06-22 10:27:27 -0400

    First commit for proof of principle for the wavelet transform. Starting with Haar, moving eventually to Dabeauchies

--- POPWavelet.py
+++ POPWavelet.py
@@ -7,3 +7,10 @@ We will be using my phantom -- with Daubeauchies and Haar wavelets.
 
 '''
 
+from __future__ import division
+import numpy as np
+import numpy.fft as fft
+from rwt import dwt, idwt
+import rwt.wavelets as wv
+import direction as d
+
