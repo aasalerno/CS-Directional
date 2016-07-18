c5e2d64d8541408ff9cad1abf31b8d272fe2667e
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-07-18 12:40:11 -0400

    Added in the rest of the code here in order to make copy pasting it easier

--- variables.py
+++ variables.py
@@ -2,13 +2,16 @@ from __future__ import division
 import numpy as np 
 import scipy as sp
 import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'gray'
 import os.path
+os.chdir('/home/asalerno/Documents/pyDirectionCompSense/')
 import transforms as tf
 import scipy.ndimage.filters
 import grads
 import sampling as samp
 import direction as d
 from scipy import optimize as opt
+
 
 filename = '/home/asalerno/Documents/pyDirectionCompSense/data/SheppLogan256.npy'
 strtag = ['spatial','spatial']
@@ -25,3 +28,43 @@ L = 2
 method = 'CG'
 dirFile = None
 nmins = None
+
+np.random.seed(2000)
+im = np.load(filename)
+
+for i in range(len(strtag)):
+    strtag[i] = strtag[i].lower()
+    
+N = np.array(im.shape) #image Size
+tupleN = tuple(N)
+pctg = 0.25 # undersampling factor
+P = 5 # Variable density polymonial degree
+
+pdf = samp.genPDF(N,P,pctg,radius = 0.1,cyl=[0]) # Currently not working properly for the cylindrical case -- can fix at home
+# Set the sampling pattern -- checked and this gives the right percentage
+k = samp.genSampling(pdf,10,60)[0].astype(int)
+
+# Diffusion information that we need
+if dirFile:
+    dirs = np.loadtxt(dirFile)
+    M = d.calc_Mid_Matrix(dirs,nmins=4)
+else:
+    dirs = None
+    M = None
+
+# Here is where we build the undersampled data
+data = np.fft.ifftshift(k)*tf.fft2c(im)
+#ph = phase_Calculation(im,is_kspace = False)
+#data = np.fft.ifftshift(np.fft.fftshift(data)*ph.conj());
+
+# IMAGE from the "scanner data"
+im_scan = tf.ifft2c(data)
+
+# Primary first guess. What we're using for now. Density corrected
+im_dc = tf.ifft2c(data/np.fft.ifftshift(pdf)).flatten().copy()
+
+# Optimization algortihm -- this is where everything culminates together
+im_result = opt.minimize(optfun, im_dc, args = (N,TVWeight,XFMWeight,data,k,strtag,dirWeight,dirs,M,nmins,scaling_factor,L),method=method,jac=derivative_fun,options={'maxiter':ItnLim,'gtol':epsilon,'disp':1})
+
+# im_result gives us a lot of information, what we really need is ['x'] reshaped to the required image size -- N
+return im_result
