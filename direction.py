Modified the code to include the correct math for Gdiffsq calculations, as well as matricies required for gradient calculations. Added diff to gTV

direction.py
@@ -68,25 +68,49 @@ def calc_Mid_Matrix(dirs,nmins):
     
     The return is an nDirs x nMins x nMins matrix where m is the number of directions that we have in the dataset.
     '''
-    
-    for i in range(30):
+    n = dirs.shape[0]
+    for i in range(n):
         if dirs[i,2] < 0:
             dirs[i,:] = -dirs[i,:]
 
         inds = dot_product_with_mins(dirs,nmins)
         #dirs = np.loadtxt(filename)
         
-        M = np.zeros([dirs.shape[0],nmins,nmins])
+        M = np.zeros([n,nmins,nmins])
         
-    for qDir in xrange(dirs.shape[0]):
+    for qDir in xrange(n):
         A = dirs[inds[qDir,:],:]-dirs[qDir,:]
         #datHold = datHold/np.linalg.norm(datHold) # Should I do this? Normalizes the vectors
         #A = np.hstack([A, datHold])
-        # I apologize for how messy this is
+        # Calculate Ahat, which is the solution to Ax = b, [(A'A)^(-1)*A']b = x
         Ahat = np.dot(inv(np.dot(A.T,A)),A.T)
         M[qDir,:,:] = np.dot(Ahat.T,Ahat)
     
-    return M
+    
+    # We need to take care of the positive and negatives of the images that we are using, since this system has a +/- aspect to the comparisons that occur
+    
+    # Make lists to hold the positive indicies and the negative ones
+    indsNeg = range(n)
+    indsPos = range(n)
+        
+    for kk in xrange(n):
+        indsNeg[kk] = [np.repeat(kk,nmins), range(nmins)]
+        indsPos[kk] = np.where(inds==kk)
+        
+    # dI, the derivative with respect to the Image. We need to now apply the +/-
+    dI = np.zeros([n,n,nmins])
+    dIM = np.zeros([n,nmins,n])
+    Ause = range(n)
+    
+    for kk in range(n):
+        dI[kk,indsNeg[kk][0],indsNeg[kk][1]] = -1
+        dI[kk,indsPos[kk][0],indsPos[kk][1]] = 1
+        Ause[kk] = np.where(np.any(dI[kk,:,:] != 0,axis=1))[0]
+        for d in xrange(len(Ause[kk])):
+            colUse = Ause[kk][d]
+            dIM[kk,:,colUse] = np.dot(dI[kk,colUse,:],M[colUse,:,:])
+    
+    return M, dIM, Ause
         
     
 #def residuals(a,b):
@@ -98,6 +122,7 @@ def least_Squares_Fitting(x,N,strtag,dirs,inds,M):
     nmins = inds.shape[1]
     dirloc = strtag.index("diff")
     x0 = np.rollaxis(x0,dirloc)
+    Gdiffsq = np.zeros(N)
     
     for q in xrange(dirs.shape[0]):
         r = inds[q,:]
@@ -111,8 +136,6 @@ def least_Squares_Fitting(x,N,strtag,dirs,inds,M):
         Irq = Ir - Iq # Iq will be taken from Ir for each part of axis 0
         #Aleft = np.linalg.solve((A.T*A),A.T)
         #beta = np.zeros(np.hstack([Iq.shape,3]))
-        
-        Gdiffsq = np.zeros(N)
         
         for i in xrange(nrow):
             for j in xrange(ncol):

grads.py
@@ -79,7 +79,18 @@ def gObj(x,N,ph,
     
     return grad
 
-def gTV(x,N,strtag,dirWeight,dirs = None,nmins = 0, M = None, p = 1,l1smooth = 1e-15):
+def gTV(x,N,strtag,dirWeight,dirs = None,nmins = 0, dirInfo = None, p = 1,l1smooth = 1e-15):
+    
+    if dirInfo:
+        M = dirInfo[0]
+        dIM = dirInfo[1]
+        Ause = dirInfo[2]
+        inds = dirInfo[3]
+    else:
+        M = None
+        dIM = None
+        Ause = None
+        inds = None
     
     x0 = x.reshape(N)
     grad = np.zeros(np.hstack([len(strtag),N]))
@@ -90,17 +101,23 @@ def gTV(x,N,strtag,dirWeight,dirs = None,nmins = 0, M = None, p = 1,l1smooth = 1
            TV_dataRoll = np.roll(TV_data[i,:,:],1,axis=i)
            grad[i,:,:] = -np.tanh(k*(TV_data[i,:,:])) + np.tanh(k*(TV_dataRoll))
        elif strtag[i] == 'diff':
-           None  # Do stuff for directional data
+           for d in xrange(N[i]):
+               
+               dDirx = np.zeros(np.hstack([N,M.shape[1]])) # dDirx.shape = [nDirs,imx,imy,nmins]
+               
+                for ind_q in xrange(N[i]):
+                    for ind_r in xrange(M.shape[1]):
+                        dDirx[ind_q,:,:,ind_r] = x0[inds[ind_q,ind_r],:,:] - x0[ind_q,:,:]
+                                      
+               
+                for comb in xrange(len(Ause[kk])):
+                    colUse = Ause[dir][comb]
+                    for qr in xrange(M.shape[1]):
+                        grad[i,d,:,:] = np.dot(dIM[d,qr,colUse],dDirx[d,:,:,qr]) + grad[i,d,:,:] 
     
     # Need to make sure here that we're iterating over the correct dimension
     # As of right now, this assumes that we're working on a slice by slice basis
     # I'll have to implement 3D data work soon.
     
-    #for i in xrange(1):
-        #Dx = tf.TV(x0,N,strtag)
-        #G = p*Dx*(Dx*Dx.conj() + l1smooth)**(p/2-1)
-        #grad = tf.iTV(G)
-        ##import pdb; pdb.set_trace()
-    #import pdb; pdb.set_trace();
     grad = np.sum(grad,axis=0)
     return grad