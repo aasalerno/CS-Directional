5189379b2bd25bff8f700a355014c02b1be43ee9
Anthony Salerno <anthony.salerno@mouseimaging.ca>
2016-08-10 13:58:08 -0400

    Changed the wavelet transform to use pywt. Need to check how to make sure the convolution for higher level wavelets don't change the size

--- transforms.py
+++ transforms.py
@@ -10,8 +10,8 @@
 from __future__ import division
 import numpy as np
 import numpy.fft as fft
-from rwt import dwt, idwt
-from rwt.wavelets import daubcqf
+import pywt
+#from rwt.wavelets import daubcqf
 import direction as d
 
 EPS = np.finfo(float).eps
@@ -24,14 +24,12 @@ def ifft2c(data_to_ifft,ph,axes=(-2,-1)):
     IFFTdata = np.sqrt(data_to_ifft.size)*fft.ifft2(data_to_ifft,axes=axes)*ph;
     return IFFTdata
 
-def xfm(data_to_xfm,scaling_factor = 2,L = 2): #Usually scaling_factor = 4, but for the haar, it's 2
-    h = daubcqf(scaling_factor)[0]
-    XFMdata = dwt(data_to_xfm,h,L)[0]
+def xfm(data_to_xfm,wavelet = 'db1'): #Usually scaling_factor = 4, but for the haar, it's 2
+    XFMdata = pywt.wavedec2(data_to_xfm,wavelet,'sym')
     return XFMdata
 
-def ixfm(data_to_ixfm,scaling_factor = 2,L = 2):
-    h = daubcqf(scaling_factor)[0]
-    IXFMdata = idwt(data_to_ixfm,h,L)[0]
+def ixfm(data_to_ixfm,wavelet = 'db1'):
+    IXFMdata = pywt.waverec2(data_to_ixfm,wavelet,'sym')
     return IXFMdata
 
 def TV(im,N,strtag,dirWeight = 1,dirs = None,nmins = 0,M=None):
@@ -79,6 +77,40 @@ def matlab_style_gauss2D(im,shape=(3,3),sigmaX = 0):
     ph = np.conj(filtdata)/(abs(filtdata)+EPS)
     
     return ph
+
+def toMatrix(x):
+    ax = []
+    for i in xrange(len(x)):
+        ax.append(xfmData[i][0].shape[0])
+        
+    N = sum(ax)
+    res = np.zeros([N,N])
+            
+    res[0:ax[0],0:ax[0]] = x[0]
+    
+    for i in xrange(1,len(ax)):
+        # Now we need to push the correct cH, cV and cD to the right spots
+        #
+        res[0:sum(ax[0:i]),sum(ax[0:i]):sum(ax[0:i+1])] = x[i][0] # cH
+        res[sum(ax[0:i]):sum(ax[0:i+1]),0:sum(ax[0:i])] = x[i][1] # cV
+        res[sum(ax[0:i]):sum(ax[0:i+1]),sum(ax[0:i]):sum(ax[0:i+1])] = x[i][2] # cD
+    
+    return res, ax
+        
+
+def fromMatrix(res,ax):
+    
+    x = []
+    
+    x.append(res[0:ax[0],0:ax[0]])
+    
+    for i in xrange(1,len(ax)):
+        x.append([])
+        x[i].append(res[0:sum(ax[0:i]),sum(ax[0:i]):sum(ax[0:i+1])]) # cH
+        x[i].append(res[sum(ax[0:i]):sum(ax[0:i+1]),0:sum(ax[0:i])]) # cV
+        x[i].append(res[sum(ax[0:i]):sum(ax[0:i+1]),sum(ax[0:i]):sum(ax[0:i+1])]) # cD
+    
+    return x
     
 #def iDx(data,shp):
    #res = data[np.hstack([0,range(shp[0]-1)]),:] - data
